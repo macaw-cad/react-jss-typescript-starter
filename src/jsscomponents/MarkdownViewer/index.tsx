7a567c8ede9dc5ae9f9886bea90bec524e26b283
Gary Wenneker <gary.wenneker@macaw.nl>
2019-04-01 08:30:42 +0200

    Hero and grid

--- src/jsscomponents/MarkdownViewer/index.tsx
+++ src/jsscomponents/MarkdownViewer/index.tsx
@@ -4,6 +4,7 @@ import Markdown from '../../components/Markdown';
 type MarkdownViewerFields = {
     body: { value: string };
     url: { value: string };
+    margin: {value: string };
 }
 type MarkdownViewerProps = {
     fields: MarkdownViewerFields;
@@ -11,24 +12,25 @@ type MarkdownViewerProps = {
 
 type MarkdownViewerState = {
     text: string;
+    margin: string;
 };
 
 class MarkdownViewer extends React.Component<MarkdownViewerProps, MarkdownViewerState> {
-    state: Readonly<MarkdownViewerState> = { text: 'Loading' };
+    state: Readonly<MarkdownViewerState> = { text: 'Loading', margin: '' };
     constructor(props: MarkdownViewerProps) {
         super(props);
         if (props.fields) {
             if (props.fields.body) {
-                this.state = { text: props.fields.body.value };
+                this.state = { text: props.fields.body.value, margin: props.fields.margin.value };
             }
             else if (props.fields.url) {
-                this.state = { text: 'Loading' };
+                this.state = { text: 'Loading', margin: props.fields.margin.value };
             }
             else {
-                this.state = { text: 'No Markdown text specified' };
+                this.state = { text: 'No Markdown text specified', margin: props.fields.margin.value };
             }
         } else {
-            this.state = { text: 'No Markdown text specified' };
+            this.state = { text: 'No Markdown text specified', margin: '' };
         }
     }
 
@@ -42,13 +44,13 @@ class MarkdownViewer extends React.Component<MarkdownViewerProps, MarkdownViewer
     }
 
     render() {
-        const { text } = this.state;
+        const { text, margin } = this.state;
         const imgBaseUrl = this.props.fields.url ? 
             this.props.fields.url.value.substring(0, this.props.fields.url.value.lastIndexOf("/")) :
             './'; //if markdown defined in body we leave urls starting with ./ the same
 
         return (
-            <Markdown body={text} imgBaseUrl={imgBaseUrl}/>         
+            <div style={{ margin: margin}}><Markdown body={text} imgBaseUrl={imgBaseUrl}/></div>         
         )
     }
 };
