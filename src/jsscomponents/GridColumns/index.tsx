c22d8c2e328061e0925a46f1bfac93d2db8d290c
Gary Wenneker <gary.wenneker@macaw.nl>
2019-04-03 08:33:40 +0200

    Added section and grid for layout

---
+++ src/jsscomponents/GridColumns/index.tsx
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import { Placeholder, withPlaceholder, withSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
+import { joinClasses } from '../../utils/Filters';
+
+interface GridColumnsProps {
+    smallColumns?: number[];
+    columns: number[];
+    largeColumns?: number[];
+    extraLargeColumns?: number[];
+    rendering?: any;
+    parameters?: any;
+    fields?: any;
+}
+
+class GridColumnsComponent extends React.Component<GridColumnsProps> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            className: "m-grid"
+        };
+    }
+    render() {
+        console.log('ok')
+        const { smallColumns, largeColumns, extraLargeColumns, parameters, rendering } = this.props;
+        if (parameters && (parameters.className !== undefined)) {
+            this.setState({
+                className: parameters.className.concat(" m-grid")
+            });
+        }
+        let columns: Array<string> = [];
+        if (
+            this.props &&
+            this.props.fields &&
+            this.props.fields.columns &&
+            this.props.fields.columns.value) {
+            if(this.props.fields.columns.value.indexOf(',') > -1) {
+                columns = this.props.fields.columns.value.split(',');
+            } else
+            {
+                columns = ['12'];
+            }
+        }
+
+        return (
+            <div className='m-grid'>
+                {columns && columns.map((column, index) => {
+                    const GridColumnClassName = joinClasses(
+                        `m-grid__M${column}`,
+                        (smallColumns !== undefined && smallColumns[index])
+                            ? `m-grid__S${smallColumns[index]}` : undefined,
+                        (largeColumns !== undefined && largeColumns[index])
+                            ? `m-grid__L${largeColumns[index]}` : "",
+                        (extraLargeColumns !== undefined && extraLargeColumns[index])
+                            ? `m-grid__XL${extraLargeColumns[index]}` : "",
+                    );
+                    return (
+                        <div key={index} className={GridColumnClassName}>
+                            {
+                                this.props[`placeholder${index+1}`]
+                            }
+                        </div>
+                    );
+                })}
+            </div >
+        )
+    }
+}
+
+const gridColumnsComponentWithPlaceholderInjected = withPlaceholder([
+    {
+        placeholder: 'jss-grid-column-1',
+        prop: 'placeholder1',
+    },
+    {
+        placeholder: 'jss-grid-column-2',
+        prop: 'placeholder2',
+    },
+    {
+        placeholder: 'jss-grid-column-3',
+        prop: 'placeholder3',
+    },
+    {
+        placeholder: 'jss-grid-column-4',
+        prop: 'placeholder4',
+    },
+    {
+        placeholder: 'jss-grid-column-5',
+        prop: 'placeholder5',
+    },
+    {
+        placeholder: 'jss-grid-column-6',
+        prop: 'placeholder6',
+    },
+    {
+        placeholder: 'jss-grid-column-7',
+        prop: 'placeholder7',
+    },
+    {
+        placeholder: 'jss-grid-column-8',
+        prop: 'placeholder8',
+    },
+    {
+        placeholder: 'jss-grid-column-9',
+        prop: 'placeholder9',
+    },
+    {
+        placeholder: 'jss-grid-column-10',
+        prop: 'placeholder10',
+    },
+    {
+        placeholder: 'jss-grid-column-11',
+        prop: 'placeholder11',
+    },
+    {
+        placeholder: 'jss-grid-column-12',
+        prop: 'placeholder12',
+    }
+]
+)(GridColumnsComponent);
+
+// We need to know if experience editor is active, to disable the dynamic tab behavior for editing.
+// Using the same technique as injecting the placeholder, we wrap the component again to inject the
+// `sitecoreContext` prop.
+const GridColumns = withSitecoreContext()(
+    gridColumnsComponentWithPlaceholderInjected
+);
+
+export default GridColumns;
