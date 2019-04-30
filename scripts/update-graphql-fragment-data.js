88120f34adafacc03b71ed8bbe6ca665454d9e44
Serge van den Oever <Serge.van.den.Oever@macaw.nl>
2019-04-30 14:06:41 +0200

    Fixed update-graphql-fragment-data

--- scripts/update-graphql-fragment-data.js
+++ scripts/update-graphql-fragment-data.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console, no-underscore-dangle */
 
-import fetch from 'isomorphic-fetch';
-import fs from 'fs';
+const fetch = require('isomorphic-fetch');
+const fs = require('fs');
 
 // Apollo Client supports caching GraphQL responses, which can greatly reduce network traffic needs.
 // In order to work correctly with interfaces in GraphQL, it needs to know some basic information about
@@ -10,7 +10,7 @@ import fs from 'fs';
 //
 // The `jss graphql:update` command should be executed when Sitecore templates related to the site are altered.
 
-const graphqlEndpoint =  `/api/${process.env.REACT_APP_SITECORE_JSS_APP_NAME}?sc_apikey=${process.env.REACT_APP_SITECORE_API_KEY}`;
+const graphqlEndpoint =  `${process.env.REACT_APP_SITECORE_API_HOST}/api/${process.env.REACT_APP_SITECORE_JSS_APP_NAME}?sc_apikey=${process.env.REACT_APP_SITECORE_API_KEY}`;
 console.log(`Updating GraphQL fragment type data from ${graphqlEndpoint}...`);
 fetch(graphqlEndpoint, {
   method: 'POST',
@@ -35,7 +35,7 @@ fetch(graphqlEndpoint, {
   .then((result) => {
     // here we're filtering out any type information unrelated to unions or interfaces
     const filteredData = result.data.__schema.types.filter((type) => type.possibleTypes !== null);
-
+    console.log(result);
     const filteredResult = { ...result };
     filteredResult.data.__schema.types = filteredData;
 
